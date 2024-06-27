6bc96ecea472c2ba77ba1c7f3d1d2a2e0ff60238
PiyumalKK <piyumalm912@gmail.com>
2024-06-27 15:34:36 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import requestRoutes from './routes/request.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 
 dotenv.config();
@@ -17,6 +18,8 @@ mongoose.connect(
 .then(
     ()=> {console.log("MongoDB is Connected!");},
 ).catch(err => {console.log(err);});
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,6 +37,12 @@ app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment',commentRoutes);
 app.use('/api/request',requestRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+});
 
 app.use((err, req, res, next)=> {
 const statusCode = err.statusCode || 500;
